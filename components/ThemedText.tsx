085851440e11c6f9c908413ae5ff559e31aae718
unknown <vinicius_23867@ETEC.FURLAN>
2025-09-05 10:56:45 -0300

    ThemedText

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,24 +1,33 @@
-import { StyleSheet, Text, type TextProps } from 'react-native';
+// O componente ThemedText é uma extensão do componente de texto padrão do React Native,
+// adicionando suporte a temas de cores e estilos de topografias predefinidos
+import { StyleSheet, Text } from 'react-native';
 
+// O useThemeColor é um hook personalizado que provavelmente determina a cor
+// com base no tema do sistema (claro ou escuro)
 import { useThemeColor } from '@/hooks/useThemeColor';
 
-export type ThemedTextProps = TextProps & {
-  lightColor?: string;
-  darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
-};
+// Define o componente ThemeText.
+// Ele aceita várias props, incluindo estilo, cores para temas claro/escuro,
+// e um "type" para estilos predefinidos
 
 export function ThemedText({
-  style,
-  lightColor,
-  darkColor,
-  type = 'default',
-  ...rest
-}: ThemedTextProps) {
+  style, // Estilo extra, que pode sobrescrever os estilos padrão
+  lightColor, // Cor para o tema claro
+  darkColor, // Cor para o tema escuro
+  type = 'default', // Tipo de estilo de texto, com 'default como padrão
+  ...rest // Coleta todas as outras props, como 'children' (o texto em si), e as repassa para o componente <Text>
+}) { 
+  // Usa o hook para obter a cor do texto com base nas cores passadas e no tema atual.
+  // Se lightColor e darkColor não forem fornecidos, ela usa a cor padrão 'text
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return (
     <Text
+    // Combina todos os estilos em um array. A ordem é importante para a
+    // sobreposição de estilos
+    // 1. A cor diâmica do tema
+    // 2. O estilo predefinido (ex: 'title', 'link') baseado na prop  'type'.
+    // 3. O estilo extra passado pela prop 'style', que tem maior propriedade.
       style={[
         { color },
         type === 'default' ? styles.default : undefined,
@@ -28,6 +37,7 @@ export function ThemedText({
         type === 'link' ? styles.link : undefined,
         style,
       ]}
+      // Repassa as demais props para o component Text ativo
       {...rest}
     />
   );
